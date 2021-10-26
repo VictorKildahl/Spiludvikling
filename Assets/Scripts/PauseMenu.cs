18626540ef5acc09b1608aae94dc337de225e038
lassemosel <lassebjoernmosel@gmail.com>
2021-10-26 11:28:20 +0200

    pause commit
    pause desciption

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public bool gameIsPaused;
+
+    public void Start(){
+        pauseMenu.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; 
+        gameIsPaused = true;
+    
+    }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f; 
+        gameIsPaused =  false;
+    }
+
+    public void Home(){
+        //  Time.timeScale = 1; 
+          Time.timeScale = 0f; 
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        //  SceneManager.LoadScene(sceneID);
+    }
+}
+ 
