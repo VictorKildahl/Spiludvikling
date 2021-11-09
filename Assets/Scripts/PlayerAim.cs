f775ad37edafa2d4a64d225226f8562ca7c38f6c
VictorKildahl <victor.petersen2@gmail.com>
2021-11-09 09:18:25 +0100

    Merge
    Merge

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    public event EventHandler<OnThrowEventArgs> onThrow;
+
+    public class OnThrowEventArgs : EventArgs
+    {
+        public Vector3 pencilEndPointPosition;
+        public Vector3 throwPosition;
+    }
+    private Transform aimTransform;
+    private Transform aimPencilEndPointTransform;
+    private Animator aimAnimator;
+    void Start()
+    {
+        aimTransform = transform.Find("Aim");
+        aimAnimator = aimTransform.GetComponent<Animator>();
+        aimPencilEndPointTransform = aimTransform.Find("Pencil");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+        RangedAttack();
+    }
+
+    private void Aim()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void RangedAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aimAnimator.SetTrigger("Throw");
+            onThrow?.Invoke(this,new OnThrowEventArgs()
+            {
+                pencilEndPointPosition = aimPencilEndPointTransform.position,
+                throwPosition = mousePos,
+            });
+        }
+    }
+}
+
+
